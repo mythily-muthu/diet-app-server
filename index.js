6300214dd7c2dc00496cd7f54d078259d7a9605c
mythily-muthu <mythilimuthu18@gmail.com>
2023-05-01 19:24:30 +0530

    Diet app initialized

---
+++ index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+
+
+//routes
+
+
+const app = express();
+
+
+let startServer = async () => {
+    try {
+        //for checking to browser;
+        app.get("/", (req, res, next) => {
+            res.status(200).send("server is running successfully");
+            console.log("server is running successfully");
+            next();
+        });
+        app.use(cors());
+        app.use(express.json());
+        //middlewares
+
+
+        const port = process.env.PORT || 3001;
+        app.listen(port, () => {
+            console.log("server is runnging at", port);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+startServer();
